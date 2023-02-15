a61fcbc13aeaa16fcf048d607d5b048bde88f4cc
Fiyin <fiyin@MacBook.local>
2023-02-15 16:27:39 -0700

    initial commit, slot machine game

---
+++ README.md
@@ -0,0 +1,38 @@
+
+<img src="https://i.imgur.com/9fBcBKl.jpg">
+
+
+# Slot Machine Game
+
+## Title: Jackpot City Casino Slot Machine
+
+The Jackpot city casino slot machine game is a game that is played in an internet browser and compactible with most browsers. The game runs like a typical slot machine with betting and spinning functions. On loading the game the player is assigned $200, with the ability to place $5 or $10 bets prior to spinning.
+
+<img src="https://i.imgur.com/GbgXBav.jpg" height="300">
+
+The game ask's the player to "Click Spin" on the slot machine after placing a bet in order to generate random values on the slot machine screen.
+
+<img src="https://i.imgur.com/FseRSXS.jpg" height="300">
+
+
+The player receives several game messages and prompts as the game progresses. The random values generated, bet placed and backend javascript logic determines the player's score and available cash throughout the game.
+
+<img src="https://i.imgur.com/TCWIhEH.jpg" width="200">
+
+### Technologies Used: 
+The technologies used in building the Jackpot city casino slot machine game include:
+- Javascript
+- HTML & 
+- CSS
+
+To get the best game experience, it is advisable to play on a laptop or desktop computer. 
+
+#### Getting Started: The link to the browser game can be found below
+
+- [Slot Machine Game]()
+
+### Next Steps: Future enhancement of the game includes:
+
+- Replacing the random number values generated on display with actual slot machine symbols.
+- Incorporating animations like a fadeIn/fadeOut process as display symbol changes.
+- 
---
+++ js/main.js
@@ -0,0 +1,176 @@
+// constants
+const display = [ 
+    {1: 'img/values/bell.jpeg'},
+    {2: 'img/values/lemon.jpeg'},
+    {3: 'img/values/orange.jpeg'},
+    {4: 'img/values/banana.jpeg'},
+    {5: 'img/values/cherries.jpeg'},
+    {6: 'img/values/water-melon.jpeg'},
+    {7: 'img/values/7.jpeg'}
+]
+
+
+// app's state (variables) 
+let score, bet, cash, betSet, spinClicked
+let value = []
+let initialized = 0
+
+
+// cached element references
+const gameMessage = document.querySelector('h2')
+let displayEl1 = document.getElementById('screen-1')
+let displayEl2 = document.getElementById('screen-2')
+let displayEl3 = document.getElementById('screen-3')
+
+const scoreEl = document.getElementById('game-score')
+const cashEl = document.getElementById('game-cash')
+const betEl = document.getElementById('gameBet')
+
+
+//event listeners
+document.getElementById('spin').addEventListener('click', playerSpin)
+document.getElementById('bet5').addEventListener('click', playerBet5)
+document.getElementById('bet10').addEventListener('click', playerBet10)
+
+
+//functions
+render()
+
+function render() {
+    if (initialized === 0) {
+        initialize()
+    }
+
+    scoreEl.innerHTML = score
+    cashEl.innerHTML = cash
+    betEl.innerHTML = bet
+
+    if (!betSet || bet === 0 ) {
+        gameMessage.textContent = "Place your bets and spin"
+    } 
+}
+
+function playerSpin(evt) {
+    let clk = evt.target
+    if (clk.localName !== 'button') {
+        return
+    }
+    if (clk.id === 'bet') {
+        return
+    } 
+    if (!spinClicked) {
+        spinClicked = true
+        getValues()
+    }
+}
+
+function gamePlay() {
+
+    if (value[0] ===  7 &&  value[1] ===  7 && value[2] === 7) {
+        cash = cash + (bet * 1000)
+        score = score + 10000
+        gameMessage.textContent = "Jackpot! You're a Winner"
+    } else if (value[0] ===  value[1] &&  value[1] ===  value[2]) {
+        cash = cash + (bet * 2)
+        score = score + 500
+        gameMessage.textContent = "Great! three out of three, Spin again."
+    } else if (value[0] === value[1] && value[1] !== value[2]) {
+        cash = cash + (bet * 1.5)
+        score = score + 200
+        gameMessage.textContent = "Great! two out of three, Spin again."
+    } else if (value[0] !== value[1] && value[1] === value[2]) {
+        cash = cash + (bet * 1.5)
+        score = score + 200
+        gameMessage.textContent = "Great! two out of three, Spin again."
+    } else if (value[0] !== value[1] && value[0] === value[2]) {
+        cash = cash + (bet * 1.5)
+        score = score + 200
+        gameMessage.textContent = "Great! two out of three, Spin again."
+    } else {
+        cash = cash - bet
+        score = score - 100
+        gameMessage.textContent = "Three different values"
+    }
+    spinClicked = false
+    updateDisplay()
+}
+
+function playerBet5(event) {
+    let clk = event.target
+    if (clk.localName !== 'button') {
+        return
+    }
+    if (clk.id === 'spin') {
+        return
+    }
+    if (!betSet) {
+        bet = 5
+        betSet = true
+    } else {
+        bet = bet + 5
+    }
+    cash = (cash -  bet)
+    gameMessage.textContent = "Click Spin"
+    updateDisplay()
+}
+
+function playerBet10(e) {
+    let clk = e.target
+    if (clk.localName !== 'button') {
+        return
+    }
+    if (clk.id === 'spin') {
+        return
+    }
+    if (!betSet) {
+        bet = 10
+        betSet = true
+    } else {
+        bet = bet + 10
+    }
+    cash = (cash -  bet)
+    gameMessage.textContent = "Click Spin"
+    updateDisplay()
+}
+
+function randomValue() {
+    return Math.floor(Math.random() * 7 + 1)
+}
+
+function getValues() {
+    if (!betSet) {
+        gameMessage.textContent = "Place your bets before spinning"
+        spinClicked = false
+        return
+    } else {
+        value[0] = randomValue()
+        value[1] = randomValue()
+        value[2] = randomValue()
+        gamePlay()
+    }
+}
+
+function updateDisplay() {
+    displayEl1.innerHTML = value[0]
+    displayEl2.innerHTML = value[1]
+    displayEl3.innerHTML = value[2]
+    render()
+}
+
+//Upon loading initialize slot machine game for different values
+function initialize() {
+    value[0] = randomValue()
+    value[1] = randomValue()
+    value[2] = randomValue()
+    if (value[0] ===  value[1] &&  value[0] ===  value[2]) {
+        initialize()
+    } else {
+        score = 0
+        cash = 200
+        bet = 0
+        betSet = false
+        spinClicked = false
+        initialized = 1
+    }
+    updateDisplay()
+}
